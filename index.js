67f56c419fd0ce8d0bb41805b564cc9cdf7919e3
Jean-Luc Geering <jeanluc.geering@ufirstgroup.com>
2015-10-13 14:45:38 +0200

    Revert "try to log in browser and in console"
    This reverts commit 819ff6073c6e3d72e9f0b018ac7d5a2a20306e22.

--- index.js
+++ index.js
@@ -120,13 +120,7 @@ var gulpSass = function gulpSass(options, sync) {
         file.contents = new Buffer(message);
         file.path = gutil.replaceExtension(file.path, '.css');
 
-        message  = gutil.colors.underline(relativePath) + '\n';
-        message += gutil.colors.gray('  ' + error.line + ':' + error.column) + '  ';
-        message += error.message;
-
-        return cb(new gutil.PluginError(
-          PLUGIN_NAME, message
-        ), file);
+        return cb(null, file);
       }
 
       message += gutil.colors.underline(relativePath) + '\n';
